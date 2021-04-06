884ed68733e470cffbcd349c85a40f5dd9e7fad8
kwameasare04 <73545744+kwameasare04@users.noreply.github.com>
2021-04-06 15:05:05 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const a = [1, 2, 3];
+const b = [1, 2, 3];
+
+//console.log(a === b); // false
+
+/* Create a function "arraysAreEqual" with two parameters "arrayOne" and "arrayTwo". 
+
+If two arrays are equal (have same quantity of the elements and all elements match e.g. arrayOne[0] === arrayTwo[0] etc.) return "true".
+
+Otherwise return "false". */
+const arraysAreEqual = (arrayOne, arrayTwo) =>{
+    if(arrayOne.length !== arrayTwo.length)
+        return false;
+    
+    return arrayOne.every((element,index) => element === arrayTwo[index]);
+}
+    
+
+
+
+
+
+const c = [2, 1, 3];
+const d = [1, 2, 3, 4];
+
+console.log(arraysAreEqual(a, b)); // true
+console.log(arraysAreEqual(a, c)); // false
+console.log(arraysAreEqual(a, d)); // false
